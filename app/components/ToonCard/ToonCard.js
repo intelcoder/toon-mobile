cb96a94652afcf5f3b85c38d9a3c6dfcde6cc084
intelcoder <seongjunkim7162@gmail.com>
2017-03-06 15:55:46 -0500

    Added ToonGrid

--- app/components/ToonCard/ToonCard.js
+++ app/components/ToonCard/ToonCard.js
@@ -14,12 +14,17 @@ import {
 export default class ToonCard extends Component {
 
   render() {
-    const {src, title, width, height} = this.props;
+    const {src, rating, author, title, width, height} = this.props;
     return (
-      <View style={{width: width, height: height}}>
-        <Image source={{uri: src}}/>
-        <View>
-          <Text>{title}</Text>
+      <View style={[styles.cardContainer,{width: width, height: height * 1.2}]}>
+        <View style={styles.imageContainer}>
+          <Image style={styles.cardImage} source={{uri: src}}/>
+        </View>
+
+        <View style={styles.textContainer}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.rating}>{rating}</Text>
+          <Text style={styles.author}>{author}</Text>
         </View>
       </View>
     )
@@ -29,13 +34,27 @@ export default class ToonCard extends Component {
 
 const styles = StyleSheet.create({
   cardContainer: {
-    flex:1,
+
   },
+  imageContainer: {flex:1},
   cardImage: {
-    flex:0.8,
-    resizeMode: 'cover'
+    flex:1,
+    resizeMode: 'stretch',
   },
   textContainer: {
-    flex:0.2
+    backgroundColor: '#f7f8f9',
+    paddingBottom: 5
+
+  },
+  title: {
+    marginTop: 3,
+    fontWeight: 'bold',
+  },
+  rating: {
+    color: 'red'
+  },
+  author: {
+    color: '#666666'
   }
+
 });
