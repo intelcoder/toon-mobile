b50c24c357c118029ced7fc379da6ae92ba9e9ae
intelcoder <seongjunkim7162@gmail.com>
2017-03-19 14:25:38 -0400

    used code deleted

--- app/components/InitializeWrapper/init.js
+++ app/components/InitializeWrapper/init.js
@@ -9,7 +9,7 @@
  */
 
 import RNFetchBlob from 'react-native-fetch-blob'
-import Model from '../../model/realm/model';
+
 
 export const saveImageToLocal = () => {
   const dirs = RNFetchBlob.fs.dirs;
@@ -26,43 +26,3 @@ export const saveImageToLocal = () => {
       });
   };
 };
-
-export const saveImageAndReturnPath = async(site, toon) => {
-    const  dirs = RNFetchBlob.fs.dirs;
-    return await RNFetchBlob
-        .config({
-            path: dirs.DocumentDir + `/${site.toLowerCase()}/${toon.toon_id}.jpg`,
-            appendExt: 'jpg',
-        })
-        .fetch('GET', toon.thumbnail_url, {})
-        .then((res) => {
-            return res.path();
-        });
-};
-
-export const saveIntoDatabase = (schema, SchemaName, data) => {
-    const webtoonModel = Model(schema);
-    webtoonModel.bulkCreate(SchemaName, data);
-};
-/*
-
-const tmp = async()=> {
-    let dirs = RNFetchBlob.fs.dirs;
-    const result = toonList.map((toon) => {
-        return RNFetchBlob
-            .config({
-                path: dirs.DocumentDir + '/naver/' + toon.toon_id + '.jpg',
-                appendExt: 'jpg',
-            })
-            .fetch('GET', toon.thumbnail_url, {})
-            .then((res) => {
-                toon.thumbnail_url = res.path();
-                return toon;
-            })
-    });
-    Promise.all(result)
-        .then((re)=> {
-            console.log("done", JSON.stringify(re))
-        })
-};
-*/
