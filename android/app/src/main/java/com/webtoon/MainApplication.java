c4e5ff6bd656cd5f974bec530c772c199808eabb
intelcoder <seongjunkim7162@gmail.com>
2017-05-10 23:09:06 -0400

    gradle cleaned and added react-vector-icon

--- android/app/src/main/java/com/webtoon/MainApplication.java
+++ android/app/src/main/java/com/webtoon/MainApplication.java
@@ -3,7 +3,9 @@ package com.webtoon;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import io.realm.react.RealmReactPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,7 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RealmReactPackage(),
+            new VectorIconsPackage(),
             new RNFetchBlobPackage()
       );
     }
