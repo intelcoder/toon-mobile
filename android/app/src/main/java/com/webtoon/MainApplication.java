c05fa854281597079f054eddba6ed68f11e377a8
intelcoder <seongjunkim7162@gmail.com>
2017-05-14 15:58:51 -0400

    duplicated line deleted

--- android/app/src/main/java/com/webtoon/MainApplication.java
+++ android/app/src/main/java/com/webtoon/MainApplication.java
@@ -5,8 +5,6 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
-import com.RNFetchBlob.RNFetchBlobPackage;
-import com.RNFetchBlob.RNFetchBlobPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -28,7 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new VectorIconsPackage(),
-            new RNFetchBlobPackage()
+            new RNFetchBlobPackage(),
       );
     }
   };
